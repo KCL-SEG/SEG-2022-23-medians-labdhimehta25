b9c395c7ec7ef1247ba750d12303b6524294ac6f
Labdhi Mehta <labdhi.1.mehta@kcl.ac.uk>
2022-10-19 23:21:35 +0100

    Correct the position of dividing the list

--- median.py
+++ median.py
@@ -8,7 +8,7 @@ while True:
         numbers = [float(value) for value in input().split(",")]
         numbers = sorted(numbers)
         if (len(numbers) % 2 == 0):
-            median = (numbers[len(numbers)//2 - 1] + numbers[len(numbers)//2] /2)
+            median = (numbers[len(numbers)//2 - 1] + numbers[len(numbers)//2]) /2
         else:
             median = numbers[len(numbers)//2]
 
