a4bb7117251afdb9b8b49e0878ad865253a01f07
Labdhi Mehta <labdhi.1.mehta@kcl.ac.uk>
2022-10-19 23:13:56 +0100

    Correct function len

--- median.py
+++ median.py
@@ -7,10 +7,10 @@ while True:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
         numbers = sorted(numbers)
-        if (numbers.len()%2 == 0):
-            median = (numbers[(numbers.len()//2) - 1] + numbers[(numbers.len()//2)+1]) /2
+        if (len(numbers) % 2 == 0):
+            median = (numbers[len(numbers)//2) - 1] + numbers[len(numbers)//2)+1]) /2
         else:
-            median = numbers[(numbers.len())// 2]
+            median = numbers[len(numbers)//2]
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
