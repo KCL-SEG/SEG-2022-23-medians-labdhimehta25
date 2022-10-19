23a9d354459d9a2b458e8861b2be171192fa0391
Labdhi Mehta <labdhi.1.mehta@kcl.ac.uk>
2022-10-19 23:24:40 +0100

    Add comments

--- median.py
+++ median.py
@@ -6,11 +6,11 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
-        numbers = sorted(numbers)
+        numbers = sorted(numbers) #sort list
         if (len(numbers) % 2 == 0):
-            median = (numbers[len(numbers)//2 - 1] + numbers[len(numbers)//2]) /2
+            median = (numbers[len(numbers)//2 - 1] + numbers[len(numbers)//2]) /2 #add the two elements next to each other when halfing the list and then half the value to work out the median
         else:
-            median = numbers[len(numbers)//2]
+            median = numbers[len(numbers)//2] #half list immediately for an odd number of elements in a list
 
     except ValueError:
         print("Some input could not be converted to a number!")
