6ffbbc7972492ee64e50c1c4799b2583b372ade8
Labdhi Mehta <labdhi.1.mehta@kcl.ac.uk>
2022-10-19 23:11:37 +0100

    Correct median variable

--- median.py
+++ median.py
@@ -2,19 +2,15 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 while True:
+    median = 0
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
-        median = 0
-        numbers.sort()
-        if (numbers.len() == 1):
-            return numbers[0]
+        numbers = sorted(numbers)
+        if (numbers.len()%2 == 0):
+            median = (numbers[(numbers.len()//2) - 1] + numbers[(numbers.len()//2)+1]) /2
         else:
-            if (numbers.len()%2 == 0):
-                median = (numbers[(numbers.len()//2) - 1] + numbers[(numbers.len()//2)+1]) /2
-            else:
-                median = numbers[(numbers.len())// 2]
-
+            median = numbers[(numbers.len())// 2]
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
