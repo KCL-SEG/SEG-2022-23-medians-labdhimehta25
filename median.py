feaba37e5e59e950c798c1aa6344b77597b8618b
Labdhi Mehta <labdhi.1.mehta@kcl.ac.uk>
2022-10-19 23:19:57 +0100

    Correct the method to find the median

--- median.py
+++ median.py
@@ -8,10 +8,10 @@ while True:
         numbers = [float(value) for value in input().split(",")]
         numbers = sorted(numbers)
         if (len(numbers) % 2 == 0):
-            median = (numbers[len(numbers)//2 - 1] + numbers[len(numbers)//2 +1] /2)
+            median = (numbers[len(numbers)//2 - 1] + numbers[len(numbers)//2] /2)
         else:
             median = numbers[len(numbers)//2]
-            
+
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
